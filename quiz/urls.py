9c6b70ea69070ac8c795d07d7bcf01803b6156ea
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-01-20 11:04:10 +0600

    Little Error Solved

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.all_quiz_view, name='all_quiz'),
+    path('all-quiz/', views.all_quiz_view, name='all_quiz'),
     path('category/<slug:category_slug>/', views.all_quiz_view, name='cat_quiz'),
     path('question/<int:q_id>/', views.question_view, name='question'),
     path('submit-answer/<int:q_id>/<int:ques_id>/', views.submit_answer_view, name='submit_answer'),
